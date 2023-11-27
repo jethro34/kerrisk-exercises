edcc308890c641d65950e9657db07a5c083d4d9f
jethro34 <jh.triana@hotmail.com>
2023-11-26 17:59:04 -0800

    bug w/ argc

--- 04-1_tee.c
+++ 04-1_tee.c
@@ -2,9 +2,34 @@
 
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[])
 {
-  
+  if (argc != 3) {
+    //printf("%d", argc);
+    printf("Usage: %s < inputfile > outputfile", argv[0]);
+    exit(0);
+  }
+
+  int infd, outfd, numBs;
+
+  if ((infd = open(argv[1], O_RDONLY)) == -1)
+    perror("failure opening input file");
+  if ((outfd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC)) == -1)
+    perror("failure opening output file");
+
+  char* c; 
+  while ((numBs = read(infd, &c, 1)) != 0) {
+    if (numBs == -1) perror("failure reading from input file");
+    else {
+      putchar(*c);
+      if ((numBs = write(outfd, &c, 1)) == -1)
+        perror("failure writing to output file");
+    }
+  }
+
   return 0;
 }
