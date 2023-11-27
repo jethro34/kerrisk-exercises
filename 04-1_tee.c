e40854e670fd7b7e267bca10b81dea832b3a48a6
jethro34 <jh.triana@hotmail.com>
2023-11-26 16:41:51 -0800

    fist push

--- 04-1_tee.c
+++ 04-1_tee.c
@@ -1,1 +1,10 @@
-// The Linux Programming Interface - M. Kerrisk * Ex 4.1/87
+// The Linux Programming Interface - M. Kerrisk * Ex 4.1/p. 87
+
+#include <fcntl.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+  
+  return 0;
+}
